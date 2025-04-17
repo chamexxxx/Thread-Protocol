14501134878268c6bde2b5368a2703990f09134f
Vyacheslav Vatlin <vyacheslav.vatlin@gmail.com>
2025-04-17 22:01:17 +0400

    create property controller attacher

---
+++ Assets/Scripts/SpellSystem/PropertyControllerAttacher.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using SpellSystem.Controllers;
+using SpellSystem.Data;
+using UnityEngine;
+
+namespace SpellSystem
+{
+    public class PropertyControllerAttacher : MonoBehaviour
+    {
+        private Dictionary<PropertyType, Type> _controllers = new()
+        {
+            { PropertyType.Large, typeof(LargePropertyController) },
+            { PropertyType.Slippery, typeof(SlipperyPropertyController) },
+            { PropertyType.Stone, typeof(StonePropertyController) },
+            { PropertyType.Glass, typeof(GlassPropertyController) },
+            { PropertyType.Frozen, typeof(FrozenPropertyController) },
+            { PropertyType.Hot, typeof(HotPropertyController) },
+            { PropertyType.Gold, typeof(GoldPropertyController) },
+            { PropertyType.Magnetic, typeof(MagneticPropertyController) },
+            { PropertyType.Steel, typeof(SteelPropertyController) }
+        };
+        
+        public void AddPropertyController(PropertyType propertyType)
+        {
+            Debug.Log("AddPropertyController: " + propertyType);
+            
+            if (_controllers.TryGetValue(propertyType, out Type controllerType))
+            {
+                if (gameObject.GetComponent(controllerType) == null)
+                {
+                    gameObject.AddComponent(controllerType);
+                }
+            }
+            else
+            {
+                Debug.LogWarning($"Нет контроллера для {propertyType}");
+            }
+        }
+        
+        public void RemovePropertyController(PropertyType propertyType)
+        {
+            if (_controllers.TryGetValue(propertyType, out Type controllerType))
+            {
+                var component = gameObject.GetComponent(controllerType);
+                
+                if (component != null)
+                {
+                    Destroy(component);
+                }
+            }
+            else
+            {
+                Debug.LogWarning($"Нет контроллера для {propertyType}");
+            }
+        }
+
+    }
+}
--- Assets/Scripts/SpellSystem/StudyableObject.cs
+++ Assets/Scripts/SpellSystem/StudyableObject.cs
@@ -19,6 +19,8 @@ namespace SpellSystem
         public bool Studed = false;
         public StudyItem StartItemData => _startItemData;
 
+        private PropertyControllerAttacher _propertyControllerAttacher;
+
         private void Start()
         {
             studySystem = FindObjectOfType<UIController>();
@@ -30,6 +32,13 @@ namespace SpellSystem
 
             _startItemData = new StudyItem(itemData);
             _initialScale = transform.localScale;
+
+            _propertyControllerAttacher = GetComponent<PropertyControllerAttacher>();
+
+            if (_propertyControllerAttacher == null)
+            {
+                _propertyControllerAttacher = gameObject.AddComponent<PropertyControllerAttacher>();
+            }
         }
         
         public bool HasProperty(PropertyType propertyType)
@@ -42,175 +51,25 @@ namespace SpellSystem
 
         public void CheckPropertyController(PropertyType propertyType)
         {
-            switch (propertyType)
-            {
-                case PropertyType.Slippery:
-                    if (!gameObject.GetComponent<SlipperyPropertyController>())
-                        gameObject.AddComponent<SlipperyPropertyController>();
-                    break;
-    
-                case PropertyType.Stone:
-                    if (!gameObject.GetComponent<StonePropertyController>())
-                        gameObject.AddComponent<StonePropertyController>();
-                    break;
-    
-                case PropertyType.Glass:
-                    if (!gameObject.GetComponent<GlassPropertyController>())
-                        gameObject.AddComponent<GlassPropertyController>();
-                    break;
-    
-                case PropertyType.Large:
-                    if (!gameObject.GetComponent<LargePropertyController>())
-                        gameObject.AddComponent<LargePropertyController>();
-                    break;
-    
-                case PropertyType.Frozen:
-                    if (!gameObject.GetComponent<FrozenPropertyController>())
-                        gameObject.AddComponent<FrozenPropertyController>();
-                    break;
-    
-                case PropertyType.Hot:
-                    if (!gameObject.GetComponent<HotPropertyController>())
-                        gameObject.AddComponent<HotPropertyController>();
-                    break;
-    
-                case PropertyType.Gold:
-                    if (!gameObject.GetComponent<GoldPropertyController>())
-                        gameObject.AddComponent<GoldPropertyController>();
-                    break;
-    
-                case PropertyType.Magnetic:
-                    if (!gameObject.GetComponent<MagneticPropertyController>())
-                        gameObject.AddComponent<MagneticPropertyController>();
-                    break;
-    
-                case PropertyType.Steel:
-                    if (!gameObject.GetComponent<SteelPropertyController>())
-                        gameObject.AddComponent<SteelPropertyController>();
-                    break;
-    
-                default:
-                    Debug.LogWarning($"Неизвестный тип свойства: {propertyType}");
-                    break;
-            }
+            Debug.Log("_propertyControllerAttacher: " + _propertyControllerAttacher);
+            
+            _propertyControllerAttacher.AddPropertyController(propertyType);
         }
 
         public void AddProperty(PropertyType propertyType)
         {
             itemData.Properties.Add(propertyType);
             
-            switch (propertyType)
-            {
-                case PropertyType.Slippery:
-                    gameObject.AddComponent<SlipperyPropertyController>();
-                    break;
-            
-                case PropertyType.Stone:
-                    gameObject.AddComponent<StonePropertyController>();
-                    break;
-            
-                case PropertyType.Glass:
-                    gameObject.AddComponent<GlassPropertyController>();
-                    break;
-            
-                case PropertyType.Large:
-                    gameObject.AddComponent<LargePropertyController>();
-                    break;
-                
-                case PropertyType.Small:
-                    gameObject.AddComponent<SmallPropertyController>();
-                    break;
-            
-                case PropertyType.Frozen:
-                    gameObject.AddComponent<FrozenPropertyController>();
-                    break;
-            
-                case PropertyType.Hot:
-                    gameObject.AddComponent<HotPropertyController>();
-                    break;
-            
-                case PropertyType.Gold:
-                    gameObject.AddComponent<GoldPropertyController>();
-                    break;
-            
-                case PropertyType.Magnetic:
-                    gameObject.AddComponent<MagneticPropertyController>();
-                    break;
-            
-                case PropertyType.Steel:
-                    gameObject.AddComponent<SteelPropertyController>();
-                    break;
-            
-                default:
-                    Debug.LogWarning($"Неизвестный тип свойства: {propertyType}");
-                    break;
-            }
+            _propertyControllerAttacher.AddPropertyController(propertyType);
             
             //Вызвать методы у контроллера
-    
         }
         
         public void RemoveProperty(PropertyType propertyType)
         {
             itemData.Properties.Remove(propertyType);
 
-            switch (propertyType)
-            {
-                case PropertyType.Slippery:
-                    RemoveComponent<SlipperyPropertyController>();
-                    break;
-        
-                case PropertyType.Stone:
-                    RemoveComponent<StonePropertyController>();
-                    break;
-        
-                case PropertyType.Glass:
-                    RemoveComponent<GlassPropertyController>();
-                    break;
-        
-                case PropertyType.Large:
-                    RemoveComponent<LargePropertyController>();
-                    break;
-                
-                case PropertyType.Small:
-                    RemoveComponent<SmallPropertyController>();
-                    break;
-        
-                case PropertyType.Frozen:
-                    RemoveComponent<FrozenPropertyController>();
-                    break;
-        
-                case PropertyType.Hot:
-                    RemoveComponent<HotPropertyController>();
-                    break;
-        
-                case PropertyType.Gold:
-                    RemoveComponent<GoldPropertyController>();
-                    break;
-        
-                case PropertyType.Magnetic:
-                    RemoveComponent<MagneticPropertyController>();
-                    break;
-        
-                case PropertyType.Steel:
-                    RemoveComponent<SteelPropertyController>();
-                    break;
-        
-                default:
-                    Debug.LogWarning($"Неизвестный тип свойства: {propertyType}");
-                    break;
-            }
-
-            void RemoveComponent<T>() where T : MonoBehaviour
-            {
-                var component = gameObject.GetComponent<T>();
-                if (component != null)
-                {
-                    Destroy(component);
-                }
-            }
-
-            
+            _propertyControllerAttacher.RemovePropertyController(propertyType);
         }
         
     }
